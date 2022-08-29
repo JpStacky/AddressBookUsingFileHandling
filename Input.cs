f29081a735bd9dfe41bbf406fc2bb5c74fd011f3
Jp <jyotiprakashpc@outlook.com>
2022-08-29 17:09:48 +0530

    Data Storing using File Handling

---
+++ Input.cs
@@ -0,0 +1,39 @@
+﻿using CsvHelper;
+using System.Globalization;
+
+namespace AddressBookFileHandling
+{
+    public class Input
+    {
+        List<Person> personData = new List<Person>();
+        Person person;
+        public void InputData()
+        {
+            person = new Person();
+            Console.Write("Enter First Name = ");
+            person.first_Name = Console.ReadLine();
+            Console.Write("Enter Last Name = ");
+            person.last_Name = Console.ReadLine();
+            Console.Write("Enter Address = ");
+            person.address = Console.ReadLine();
+            Console.Write("Enter State = ");
+            person.state = Console.ReadLine();
+            Console.Write("Enter City = ");
+            person.city = Console.ReadLine();
+            Console.Write("Enter Zip Code = ");
+            person.zip = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Phone Number = ");
+            person.phone_Number = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Enter Email = ");
+            person.email = Console.ReadLine();
+
+            personData.Add(person);
+
+            using (StreamWriter writer = new StreamWriter(@"C:\source\repos\BridgeLabz\CSharp\AddressBookFileHandling\AddressBookFileHandling\Data.csv"))
+            using (CsvWriter csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(personData);
+            }
+        }
+    }
+}
